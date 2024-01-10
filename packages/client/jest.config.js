fcc16e7c363c918c4f1ec915d3c07c8f91c4bc51
Pamella Bezerra <pamella@vinta.com.br>
2024-01-10 18:15:31 -0300

    Remove unknown key

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -2,7 +2,6 @@ import base from '../../jest.config.base.js';
 
 export default {
   ...base,
-  name: 'client',
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['../../test-setup.ts'],
   setupFilesAfterEnv: ['../../test-setup-after-env.ts'],
--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,7 +2,6 @@ import base from '../../jest.config.base.js';
 
 export default {
   ...base,
-  name: 'server',
   testEnvironment: 'node',
   setupFiles: ['../../test-setup.ts'],
   setupFilesAfterEnv: ['../../test-setup-after-env.ts'],
