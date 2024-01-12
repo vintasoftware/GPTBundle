86f276dab1a49411b9a71a4f09df2deac6a6f402
Pamella Bezerra <pamella@vinta.com.br>
2024-01-12 16:01:49 -0300

    Use MUI on gen er page

--- docs/src/app/examples/generation/entity-relationship/page.tsx
+++ docs/src/app/examples/generation/entity-relationship/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { Text, Box, Button, Textarea, Title, LoadingOverlay, Loader, Center, Code } from '@mantine/core';
+import { useState } from 'react';
+import { Link, Stack, TextField, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
 import { RJSFSchema } from '@rjsf/utils';
 import { IChangeEvent } from '@rjsf/core';
-import { useState } from 'react';
-import { IconForms } from '@tabler/icons-react';
 import dedent from 'dedent';
 
 import { useGeneratedFormSchema } from '@ai-form-toolkit/client';
+import LoadingBackdrop from '@/components/Examples/LoadingBackdrop';
 import SchemaFormDemo from '@/components/Forms/SchemaFormDemo';
 
 import '../styles.css';
@@ -43,61 +45,54 @@ export default function ERSchemaGenExample() {
   };
 
   return (
-    <>
-      <Box pos="relative" mb="md">
-        <LoadingOverlay
-          visible={isLoading}
-          loaderProps={{
-            children: (
-              <>
-                <Center>
-                  <Loader size={16} mr={8} />
-                  <Text inherit>Loading (may take up to 2 minutes)...</Text>
-                </Center>
-              </>
-            ),
-          }}
-        />
-        <Title order={2} mb="xs">
+    <Stack spacing={2}>
+      <Stack spacing={2}>
+        <LoadingBackdrop open={isLoading} />
+
+        <Typography variant="h4">
           Generate forms from{' '}
-          <a href="https://mermaid.js.org/syntax/entityRelationshipDiagram.html" target="_blank" rel="noreferrer">
+          <Link href="https://mermaid.js.org/syntax/entityRelationshipDiagram.html" target="_blank" underline="hover">
             Mermaid ER diagrams
-          </a>
+          </Link>
           :
-        </Title>
-        <Text mb="xs">
-          Code at <Code>docs/src/app/examples/generation/entity-relationship/page.tsx</Code>
-        </Text>
-        <Textarea
+        </Typography>
+        <Typography variant="body1">
+          Code at <code>docs/src/app/examples/generation/entity-relationship/page.tsx</code>
+        </Typography>
+
+        <TextField
+          multiline
+          fullWidth
           label="Mermaid diagram for Entity-Relationship Model:"
           placeholder="Put Mermaid text here..."
           value={content}
-          autosize
           minRows={6}
           onChange={(event) => setContent(event.currentTarget.value)}
-          mb="md"
         />
-        <Textarea
+        <TextField
+          multiline
+          fullWidth
           label="Prompt:"
           placeholder={`e.g. ${defaultPrompt}`}
           value={prompt}
           onChange={(event) => setPrompt(event.currentTarget.value)}
-          mb="md"
         />
-      </Box>
-      <Button
-        leftSection={isLoading ? <Loader color="blue" size={14} /> : <IconForms size={14} />}
-        variant="outline"
+      </Stack>
+
+      <LoadingButton
+        variant="outlined"
+        loadingPosition="start"
+        loading={isLoading}
+        startIcon={<ListAltOutlinedIcon />}
         onClick={() => {
           setIsLoading(true);
           generateFormSchema(content, prompt).finally(() => setIsLoading(false));
         }}
-        disabled={isLoading}
-        mb="md"
+        sx={{ alignSelf: 'flex-end' }}
       >
         Generate Form
-      </Button>
+      </LoadingButton>
       <SchemaFormDemo formSchema={formSchema} uiSchema={uiSchema} onSubmit={onSubmit} />
-    </>
+    </Stack>
   );
 }
