309fb10e14255442c9ae2bb5fc13571c6f536692
Pamella Bezerra <pamella@vinta.com.br>
2024-01-12 18:22:40 -0300

    Create MuiThemeWrapper

---
+++ docs/src/MuiThemeWrapper.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const MuiThemeWrapper = ({ children }: { children: ReactNode }) => {
+  const theme = createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: 'none', // Disable auto-capitalization of button text
+          },
+        },
+      },
+    },
+  });
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default MuiThemeWrapper;
