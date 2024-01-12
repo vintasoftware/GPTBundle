b4c81b4758f960f035df672c6865b0f6787c6dbc
Pamella Bezerra <pamella@vinta.com.br>
2024-01-12 12:36:35 -0300

    Use MUI on multi field assistant page

--- docs/src/app/examples/assistant/multi-field-assistant/page.tsx
+++ docs/src/app/examples/assistant/multi-field-assistant/page.tsx
@@ -1,21 +1,10 @@
 'use client';
 
-import {
-  Autocomplete,
-  Button,
-  Group,
-  Loader,
-  TextInput,
-  Textarea,
-  Title,
-  Text,
-  Code,
-  ActionIcon,
-  List,
-} from '@mantine/core';
-import { IconBrain, IconWand } from '@tabler/icons-react';
-import { useForm } from '@mantine/form';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Autocomplete, Box, Button, CircularProgress, IconButton, TextField, Typography, Stack } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import AutoFixHighOutlinedIcon from '@mui/icons-material/AutoFixHighOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
 
 import { useFormAssistant } from '@ai-form-toolkit/client';
 
@@ -23,22 +12,17 @@ const CATEGORY_CHOICES = ['Bug', 'Feature', 'Improvement'];
 const PRIORITY_CHOICES = ['High', 'Medium', 'Low'];
 
 export default function MultiFieldFormAssistant() {
-  const form = useForm({
-    initialValues: {
-      title: 'Fix N+1s in Django codebase',
-      description: 'Use prefetch...',
-      category: '',
-      priority: '',
-    },
-    validate: {
-      category: (value) => (CATEGORY_CHOICES.includes(value) ? null : 'Pick a valid category'),
-      priority: (value) => (PRIORITY_CHOICES.includes(value) ? null : 'Pick a valid priority'),
-    },
+  const [formValues, setFormValues] = useState<Record<string, string>>({
+    title: 'Fix N+1s in Django codebase',
+    description: 'Use prefetch...',
+    category: '',
+    priority: '',
   });
+
   const { fillSingleField, fillFields } = useFormAssistant({
     formTitle: 'Create Issue',
-    formGetValues: () => form.values,
-    formSetValues: form.setValues,
+    formGetValues: () => formValues,
+    formSetValues: setFormValues,
     fieldChoices: {
       category: CATEGORY_CHOICES,
       priority: PRIORITY_CHOICES,
@@ -47,92 +31,122 @@ export default function MultiFieldFormAssistant() {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadingDescription, setIsLoadingDescription] = useState(false);
 
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
   return (
-    <>
-      <Title mb="md" order={2}>
-        GPT-4 form assistant (multi field)
-      </Title>
-      <Text mb="xs">
-        Code at <Code>docs/src/app/examples/assistant/multi-field-assistant/page.tsx</Code>
-      </Text>
-      <Text mb="xs">
-        This form uses the <Code>useFormAssistant</Code> hook to enable the GPT-4 autofill functionality in two ways:
-      </Text>
-      <List type="ordered" mb="xs">
-        <List.Item>
+    <Stack spacing={2}>
+      <Typography variant="h4">GPT-4 form assistant (multi field)</Typography>
+      <Typography variant="body1">
+        Code at <code>docs/src/app/examples/assistant/multi-field-assistant/page.tsx</code>
+      </Typography>
+      <Typography variant="body1">
+        This form uses the <code>useFormAssistant</code> hook to enable the GPT-4 autofill functionality in two ways:
+      </Typography>
+
+      <Box component="ol">
+        <Typography component="li" variant="body1">
           The &quot;magic wand&quot; button on description field can enhance whatever text you put there.
-        </List.Item>
-        <List.Item>
+        </Typography>
+        <Typography component="li" variant="body1">
           The &quot;Autofill missing with GPT-4&quot; button will fill all the missing fields in the form.
-        </List.Item>
-      </List>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput
-          withAsterisk
-          label="Title"
-          placeholder="Write the title here"
-          {...form.getInputProps('title')}
-          disabled={isLoading}
-          mb="xs"
-        />
-        <Textarea
-          withAsterisk
-          label="Description"
-          placeholder="Write the description here"
-          autosize
-          minRows={4}
-          rightSection={
-            <ActionIcon
-              variant="outline"
-              color="grey"
-              aria-label="Enhance"
-              loading={isLoadingDescription}
+        </Typography>
+      </Box>
+
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2}>
+          <TextField
+            required
+            fullWidth
+            label="Title"
+            placeholder="Write the title here"
+            disabled={isLoading}
+            value={formValues.title}
+            onChange={handleInputChange}
+          />
+          <TextField
+            required
+            multiline
+            fullWidth
+            label="Description"
+            placeholder="Write the description here"
+            minRows={4}
+            value={formValues.description}
+            onChange={handleInputChange}
+            disabled={isLoading}
+            InputProps={{
+              endAdornment: (
+                <IconButton
+                  aria-label="Enhance"
+                  size="large"
+                  disabled={isLoadingDescription}
+                  onClick={() => {
+                    setIsLoadingDescription(true);
+                    fillSingleField('description').finally(() => setIsLoadingDescription(false));
+                  }}
+                >
+                  {isLoadingDescription ? <CircularProgress size={20} color="inherit" /> : <AutoFixHighOutlinedIcon />}
+                </IconButton>
+              ),
+            }}
+          />
+          <Autocomplete
+            options={CATEGORY_CHOICES}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                required
+                label="Category"
+                placeholder="Pick a category"
+                value={formValues.category}
+                onChange={handleInputChange}
+                disabled={isLoading}
+              />
+            )}
+          />
+          <Autocomplete
+            options={PRIORITY_CHOICES}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                required
+                label="Priority"
+                placeholder="Pick a priority"
+                value={formValues.priority}
+                onChange={handleInputChange}
+                disabled={isLoading}
+              />
+            )}
+          />
+
+          <Stack direction="row" spacing={2} alignSelf="flex-end">
+            <LoadingButton
+              variant="outlined"
+              loadingPosition="start"
+              loading={isLoading}
+              startIcon={<PsychologyOutlinedIcon />}
               onClick={() => {
-                setIsLoadingDescription(true);
-                fillSingleField('description').finally(() => setIsLoadingDescription(false));
+                setIsLoading(true);
+                fillFields().finally(() => setIsLoading(false));
               }}
             >
-              <IconWand style={{ width: '70%', height: '70%' }} stroke={1.5} />
-            </ActionIcon>
-          }
-          {...form.getInputProps('description')}
-          disabled={isLoading}
-          mb="xs"
-        />
-        <Autocomplete
-          withAsterisk
-          label="Category"
-          placeholder="Pick a category"
-          data={CATEGORY_CHOICES}
-          {...form.getInputProps('category')}
-          disabled={isLoading}
-          mb="xs"
-        />
-        <Autocomplete
-          withAsterisk
-          label="Priority"
-          placeholder="Pick a priority"
-          data={PRIORITY_CHOICES}
-          {...form.getInputProps('priority')}
-          disabled={isLoading}
-          mb="xs"
-        />
-
-        <Group justify="flex-end" mt="md">
-          <Button
-            variant="default"
-            leftSection={isLoading ? <Loader size={14} /> : <IconBrain size={14} />}
-            disabled={isLoading}
-            onClick={() => {
-              setIsLoading(true);
-              fillFields().finally(() => setIsLoading(false));
-            }}
-          >
-            Autofill missing with GPT-4
-          </Button>
-          <Button type="submit">Submit</Button>
-        </Group>
+              Autofill missing with GPT-4
+            </LoadingButton>
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
       </form>
-    </>
+    </Stack>
   );
 }
