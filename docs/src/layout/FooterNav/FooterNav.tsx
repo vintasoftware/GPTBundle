8ed33ed8959e681939c719befbd9026f5c2f4b61
Pamella Bezerra <pamella@vinta.com.br>
2024-01-15 12:39:00 -0300

    Fix footer styling

--- docs/src/layout/FooterNav/FooterNav.tsx
+++ docs/src/layout/FooterNav/FooterNav.tsx
@@ -2,7 +2,7 @@ import { Box, Link, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 const FooterNav = () => (
-  <Box sx={{ position: 'sticky', bottom: 0, backgroundColor: '#fff', borderTop: `1px solid ${grey[300]}` }}>
+  <Box sx={{ position: 'sticky', bottom: 0, backgroundColor: '#fff', borderTop: `1px solid ${grey[300]}`, zIndex: 1 }}>
     <Box component="footer" padding={3}>
       <Typography variant="body2" display="flex" alignItems="center" gap={0.5}>
         © 2024
