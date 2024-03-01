7b68478b404e514c576cfed3475dadf875aae979
Felipe Farias <felipe@vinta.com.br>
2024-03-01 11:18:48 -0300

    Implement requestDialog for error handling with invalid OpenAI Keys.

--- docs/src/app/examples/assistant/single-field-assistant/page.tsx
+++ docs/src/app/examples/assistant/single-field-assistant/page.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-import { ChangeEvent, FormEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { Button, TextField, Typography, Stack } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import TipsAndUpdatesOutlinedIcon from '@mui/icons-material/TipsAndUpdatesOutlined';
-
+import { useRequestDialog } from '../../../requestDialog'; // Make sure this path is correct
 import { useFormAssistant } from '@ai-form-toolkit/client';
 
 export default function SingleFieldFormAssistant() {
+  const { requestDialog, renderDialog, isLoading } = useRequestDialog();
+
   const [formValues, setFormValues] = useState<Record<string, string>>({
     recipe: 'Carrot cake recipe\n\nIngredients:\n\nDirections:\n\n',
   });
+
   const { fillSingleField } = useFormAssistant({
     formTitle: 'Create Recipe',
     formGetValues: () => formValues,
     formSetValues: setFormValues,
   });
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setFormValues({
@@ -27,20 +29,11 @@ export default function SingleFieldFormAssistant() {
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log(formValues);
   };
 
   return (
     <Stack spacing={2}>
       <Typography variant="h4">GPT-4 form assistant (single field)</Typography>
-      <Typography variant="body1">
-        Code at <code>docs/src/app/examples/assistant/single-field-assistant/page.tsx</code>
-      </Typography>
-      <Typography variant="body1">
-        This form uses the <code>useFormAssistant</code> hook to enable the GPT-4 autofill functionality. Click on the
-        button below to test it!
-      </Typography>
-
       <form onSubmit={handleSubmit}>
         <Stack spacing={2}>
           <TextField
@@ -60,21 +53,19 @@ export default function SingleFieldFormAssistant() {
             <LoadingButton
               variant="outlined"
               loadingPosition="start"
-              loading={isLoading}
               startIcon={<TipsAndUpdatesOutlinedIcon />}
-              onClick={() => {
-                setIsLoading(true);
-                fillSingleField('recipe').finally(() => setIsLoading(false));
-              }}
+              loading={isLoading}
+              onClick={() => requestDialog(() => fillSingleField('recipe'))}
             >
               Autofill with GPT-4
             </LoadingButton>
-            <Button type="submit" variant="contained">
+            <Button type="submit" variant="contained" disabled={isLoading}>
               Submit
             </Button>
           </Stack>
         </Stack>
       </form>
+      {renderDialog()}
     </Stack>
   );
 }
---
+++ docs/src/app/requestDialog.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { styled } from '@mui/material/styles';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiPaper-root': {
+    position: 'absolute',
+    top: theme.spacing(5),
+    backgroundColor: 'rgb(10, 6, 26)',
+  },
+}));
+
+export const useRequestDialog = () => {
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const requestDialog = async (requestFunction: () => Promise<void>) => {
+    setIsLoading(true);
+    try {
+      await requestFunction();
+    } catch (error: unknown) {
+      setOpen(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const renderDialog = () => (
+    <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+      <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+        OpenAI API key is required
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent dividers sx={{ padding: '30px 16px!important' }}>
+        <Typography gutterBottom>
+          To fully access this form, an OpenAI API key is required. Please download the code and input an OpenAI API key
+          in your local environment.
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          sx={{ height: '40px', paddingTop: '12px', paddingBottom: '12px', fontWeight: 600 }}
+          startIcon={<GitHubIcon />}
+          variant="outlined"
+          target={'_blank'}
+          href={'https://github.com'}
+        >
+          Visit GitHub
+        </Button>
+      </DialogActions>
+    </BootstrapDialog>
+  );
+
+  return { requestDialog, renderDialog, isLoading };
+};
